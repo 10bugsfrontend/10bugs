efea21fc138a40e4337be74fdf82502bdd6e359d
10bugsfrontend <57103529+10bugsfrontend@users.noreply.github.com>
2020-01-19 13:42:24 +0200

    Demo @10bugs

---
+++ target/classes/static/popupPlata.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+function cancelFunction() {
+	  var popup = document.getElementById("anulare");
+	  popup.classList.toggle("show");
+	}
+function overdueFunction() {
+	  var popup = document.getElementById("overduePopup");
+	  popup.classList.toggle("show");
+	}
+function completedFunction() {
+	  var popup = document.getElementById("completedPopup");
+	  popup.classList.toggle("show");
+	}
